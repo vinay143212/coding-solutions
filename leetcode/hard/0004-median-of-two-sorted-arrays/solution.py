cc46f3b838b0536e0a0686341ad920aad336b1df
vinay s <vvinu0195@gmail.com>
2026-08-16 16:00:32 +0530

    ✅ Median of Two Sorted Arrays | Hard | Python | Runtime: 4 ms (28.40%) | Memory: 19.5 MB | 2026-08-16

---
+++ leetcode/hard/0004-median-of-two-sorted-arrays/solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        arr = nums1 + nums2
+        arr.sort()
+
+        n = len(arr)
+
+        if n % 2 == 1:
+            return arr[n // 2]
+        else:
+            return (arr[n // 2 - 1] + arr[n // 2]) / 2
